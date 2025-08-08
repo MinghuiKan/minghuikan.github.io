8326200a3041883dd131dd740668bb1119689954
MinghuiKan <z5507700@ad.unsw.edu.au>
2025-08-08 15:45:43 +1000

    Delete ass directory

--- ass/java.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.emotion-color').forEach(block => {
-    block.addEventListener('click', () => {
-        const emotionText = block.getAttribute('data-emotion');
-        document.getElementById('emotion-display').textContent = `Emotion: ${emotionText}`;
-    });
-});
