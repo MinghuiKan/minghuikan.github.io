eee392d189597b05b525e056c7c8a35e4a7a01f4
MinghuiKan <z5507700@ad.unsw.edu.au>
2025-08-08 15:48:54 +1000

    Add files via upload

---
+++ java.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.emotion-color').forEach(block => {
+    block.addEventListener('click', () => {
+        const emotionText = block.getAttribute('data-emotion');
+        document.getElementById('emotion-display').textContent = `Emotion: ${emotionText}`;
+    });
+});
